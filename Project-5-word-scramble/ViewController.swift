848b29919d78b49d138d1a466a947246c142d3da
kevin cuadros <kevindavidcuadros@gmail.com>
2024-08-07 12:11:13 -0500

    read a txt from disk, separate the string to save in array and start game selected a ramdom word

--- Project-5-word-scramble/ViewController.swift
+++ Project-5-word-scramble/ViewController.swift
@@ -8,10 +8,43 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    
+    var allWords = [String]()
+    var usedWords = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt"){
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        } 
+        
+        if allWords.isEmpty {
+            allWords = ["solkworm"]
+        }
+        
+        startGame()
+        
+    }
+    
+    func startGame() {
+        self.title = allWords.randomElement()
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = usedWords[indexPath.row]
+        cell.contentConfiguration = content
+        return cell
     }
 
 
